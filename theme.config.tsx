69198e059dfda319a746e6f25fc58447e40b4163
Weaver <95450946+Enjoyweaver@users.noreply.github.com>
2023-09-16 14:00:11 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,24 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-    head: () => {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { frontMatter } = useConfig()
-    const url =
-      'https://my-app.com' +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
- 
-    return (
-      <>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || 'UF'} />
-        <meta
-          property="og:description"
-          content={frontMatter.description || 'The next site builder'}
-        />
-      </>
-    )
-  }
+    head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="UF" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  )
   logo: <span>Uberrimae Fidei</span>,
   project: {
     link: 'https://github.com/indemnifi',
